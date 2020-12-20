cf54a8b08175bedfdd5c32a19026b299386aa4e4
Rob Martin <rmartin3@alumni.unca.edu>
2020-12-20 13:30:31 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderStopWatchApp = (containerId) => {
+  ReactDOM.render(<App />, document.getElementById(containerId));
+  // serviceWorker.unregister();
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.unmountStopWatchApp = (containerId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById("StopWatchApp-container")) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+  // serviceWorker.unregister();
+}
